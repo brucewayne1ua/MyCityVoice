b4845ab007dd13bd159c6da94f1c448cdb5ec66f
Rustam Ohli <adamsonrustam@gmail.com>
2026-02-20 23:12:57 +0100

    Add Review

---
+++ src/main/java/com/MyCityVoice/proj/model/Review.java
@@ -0,0 +1,9 @@
+package com.MyCityVoice.proj.model;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+
+@Entity
+@Table (name = "city")
+public class Review {
+    //Add Entity of Reviews
+}
